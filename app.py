83e2df4d9801a950aa4d0aa36959cb51ecf78c5d
Aubrey Graves <grav0165@gmail.com>
2023-10-20 16:43:08 -0500

    learning int function to change input strint to int variable

--- app.py
+++ app.py
@@ -20,5 +20,9 @@
 # patient_age = 20
 # new_patient = True
 
-name = input("What is your name? ")
-print("Hello, " +  name + " how are you today?")
+# name = input("What is your name? ")
+# print("Hello, " +  name + " how are you today?")
+
+birth_year = input("enter your birth year: ")
+age = 2023 - int(birth_year)
+print(age)
