30b2e39352128e14de7e792d7828a5bb61c94d75
Aubrey Graves <grav0165@gmail.com>
2023-10-20 16:37:01 -0500

    creating first lesson

---
+++ app.py
@@ -0,0 +1,12 @@
+# show printing to console
+print("Hello world")
+
+# show variables re-assigned
+age = 20
+age = 30
+# printing variable
+print(age)
+
+first_name = "Aubrey"
+is_online = False
+print = 19.95
